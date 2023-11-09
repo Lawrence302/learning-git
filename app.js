d79696d8d243ff3d87fa5c61b1d4f9e5510c55da
Lawrence302 <90982209+Lawrence302@users.noreply.github.com>
2023-11-10 03:26:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ function addNum(a,b){
 
 // function to devide two numbers
  const devide = (a,b) => a/b;
+
+// another line to be added
+// funtion to multiply two numbers using arrow function
+  const multiply = (a,b) => a * b;
+
+// funtion to devide two numbers using arrow function
+  const multiply = (a,b) => a / b;
+
